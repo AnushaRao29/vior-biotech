ce14d8d48a6a35d789510396d15404b4482868b0
Anusha Shivashankar Rao <anushashivashankarrao@Anushas-MacBook-Air.local>
2025-06-11 03:53:27 +0530

    fix: remove-comments

--- src/pages/Clientele.jsx
+++ src/pages/Clientele.jsx
@@ -42,7 +42,6 @@ export const Clientele = () => {
           className="relative max-w-6xl w-full flex flex-col md:flex-row items-center md:items-start gap-10"
           data-aos="fade-up"
         >
-          {/* Text Card Overlapping */}
           <div className="w-full mx-auto  bg-white/10 backdrop-blur-lg rounded-2xl shadow-2xl p-10 border border-white/20 z-10">
             <h2 className="text-center text-3xl md:text-4xl mb-10">
               {Texts.clientele.title}
--- src/pages/about/Overview.jsx
+++ src/pages/about/Overview.jsx
@@ -20,7 +20,6 @@ export const Overview = () => {
           className="relative max-w-6xl w-full flex flex-col md:flex-row items-center md:items-start gap-10"
           data-aos="fade-up"
         >
-          {/* Text Card Overlapping */}
           <div className="w-3/4 mx-auto  bg-white/10 backdrop-blur-lg rounded-2xl shadow-2xl p-10 border border-white/20 z-10">
             <h2 className="text-3xl md:text-4xl mb-6 drop-shadow-md">
               {Texts.overview.title1}
